4ff6925b0e620a94691999d79c592c6b7dc1d66f
sonikaPinto <sonikapinto173@gmail.com>
2020-09-05 13:38:38 +0000

    list_delete_endpoint

--- delete_idle_vm.py
+++ delete_idle_vm.py
@@ -1,26 +1,37 @@
-import os
-import time
 import googleapiclient.discovery
 from google.oauth2 import service_account
+from flask import Flask, json
+import os
+import time
 
+api = Flask(__name__)
 
-# [START list_instances]
-def list_instances(compute, project, zone):
+#Globals
+credentials = service_account.Credentials.from_service_account_file('./CREDENTIALS_FILE.json')
+compute = googleapiclient.discovery.build('compute', 'v1',credentials=credentials)
+project = "bhanu-k8s-proj"
+zone = "asia-east1-a"
+name = "vm-kube-master-0"
+bucket = "bhanu-k8s-proj"
+
+#Home
+@api.route('/', methods=['GET'])
+def home():
+    return "Welcome,you are on!"
+
+@api.route('/list', methods=['GET'])
+def list_instances():
     result = compute.instances().list(project=project, zone=zone).execute()
-    return result['items'] if 'items' in result else None
-# [END list_instances]
+    return result['items'] if 'items' in result else "No_items"
 
-
-# [START delete_instance]
-def delete_instance(compute, project, zone, name):
+#Delete Instance
+def delete_instance(compute, project, zone, instance):
     return compute.instances().delete(
         project=project,
         zone=zone,
-        instance=name).execute()
-# [END delete_instance]
+        instance=instance).execute()
 
-
-# [START wait_for_operation]
+#wait for deletion
 def wait_for_operation(compute, project, zone, operation):
     print('Waiting for operation to finish...')
     while True:
@@ -28,38 +39,31 @@ def wait_for_operation(compute, project, zone, operation):
             project=project,
             zone=zone,
             operation=operation).execute()
-
         if result['status'] == 'DONE':
             print("done.")
             if 'error' in result:
                 raise Exception(result['error'])
-            return result
-
+            return "DELETE SUCCESSFUL"
         time.sleep(1)
-# [END wait_for_operation]
 
-
-# [START run]
-def main(project, bucket, zone, instance_name, wait=True):
-    credentials = service_account.Credentials.from_service_account_file('./CREDENTIALS_FILE.json')
-    compute = googleapiclient.discovery.build('compute', 'v1',credentials=credentials)
-    instances = list_instances(compute, project, zone)
-    if instances is not None:
-      for instance  in instances:
-        operation = delete_instance(compute, project, zone, instance["id"])
-        wait_for_operation(compute, project, zone, operation['name'])        
-
-      print('Instances in project %s and zone %s:' % (project, zone))
-      for instance in instances:
-        print(' - ' + instance['name'] + instance["id"])
-        output+=instance['name']+"||"+instance["id"]+"--"
-      return output
+@api.route('/delete/', methods=['GET'])
+def main():
+    instances = list_instances()
+    output = ""
+    if not instances=="No_items":
+        for instance  in instances:   
+          print instance
+          operation = delete_instance(compute, project, zone, instance["id"])
+          result = wait_for_operation(compute, project, zone, operation['name'])        
+        print('Instances in project %s and zone %s:' % (project, zone))
+        for instance in instances:
+          print(' - ' + instance['name'] + instance["id"])
+          output+=instance['name']+"||"+instance["id"]+"--"
+        return output
     else:
-      print("no Hosts present")
-      return "None instances present"
-
-# [END run]
-
+        print("no Hosts present")
+        return "No instances present"
 
 if __name__ == '__main__':
-    main("bhanu-k8s-proj", "bhanu-k8s-proj", "asia-east1-a", "vm-kube-master-0")
+    api.run(host='127.0.0.1', port=8001)
+    
